785fb896941d0d99c8c28fe6e14e8e23637c4d16
Diyar Faraj <diyar.faraj@gmail.com>
2020-03-13 13:23:45 +0100

    connected to the api and initial values are visible

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = '0abb5d52';
+	const APP_KEY = '309694251b3825b7d3a2509c161bc550';
+
+	const [ recipes, setRecipes ] = useState([]);
+
+	useEffect(() => {
+		getRecipes();
+	}, []);
+
+	const getRecipes = async () => {
+		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+		const data = await response.json();
+
+		console.log(data.hits);
+		setRecipes(data.hits);
+	};
+
+	return (
+		<div className="App">
+			<form action="" className="search-form">
+				<input className="search-bar" type="text" />
+				<button className="search-btn" type="submit">
+					Search
+				</button>
+			</form>
+
+			{recipes.map((recipe) => (
+				<Recipe title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} />
+			))}
+		</div>
+	);
+};
 
 export default App;
